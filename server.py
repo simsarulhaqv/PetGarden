4249b27893ee7b2e1744df28b687eb1d4e5611b8
roshan <roshan22@gmail.com>
2015-06-13 15:01:03 +0530

    comments are added

--- server.py
+++ server.py
@@ -20,7 +20,8 @@ if con.execute(''' CREATE TABLE Datas
 	        	print "Table created succesfully"		
 else:
 	print "Table already exist"
-port=serial.Serial("/dev/ttyACM0",9600)
+port=serial.Serial("/dev/ttyACM0",9600) # change according to change in port us "ls /dev/" for this pi numbr 50 has ttyACM0
+                                        # for upper most USB port
 if port:
 	print "opened port succesfully"
 	while 1:
@@ -33,7 +34,7 @@ if port:
 
 			
 		print 'waiting...'
-		rcv=port.readline()
+		rcv=port.readline() #rcv is for recieving data
 		rcv=rcv.strip()
 		print "recieved data :",rcv
 		if len(rcv)>0:
@@ -41,13 +42,13 @@ if port:
 			humidt=int(rcv[3:4])
 			moistr=int(rcv[5:6])
 			lite=int(rcv[7:8]) 
-			wc=int(rcv[9])
+			wc=int(rcv[9]) #wc is water content
 			rtime=datetime.datetime.now()
 			con.execute('''' INSERT INTO Datas
 				(TIME,TEMP,HUMID,MOIST,LIGHT,WATER)
 				VALUES(rtime,temptr,humidt,moistr,lite,wc)
 					)
-				''');
+				''');# WATER is water content
 			con.commit()
 con.close()
 port.close()			
