8e12a9183e265739627319a2f2bbc5a346e7d476
roshantac <muhammedroshan222@gmail.com>
2015-06-15 16:26:38 +0530

    Update server.py
    added leaf moisture surface temparature

--- server.py
+++ server.py
@@ -14,7 +14,8 @@ if con.execute(''' CREATE TABLE Datas
 	         HUMID INTEGER,
 	         MOIST INTEGER,
 	         LIGHT INTEGER,
-	         WATER INTEGER,
+	         SURFTEMP INTEGER,
+	         LEAFMOIST INTEGER,
 	         RTIME INTEGER
 	        	);'''):
 	        	print "Table created succesfully"		
@@ -28,23 +29,26 @@ if port:
 		k=con.execute(''' SELECT RTIME FROM Datas WHERE SNO=(SELECT MAX(SNO)  FROM Datas)''')
 		
 		if(k):
-			port.write(k)
-			
+			snd="x" + str (k) + "y"  #this is the recieving format kp want
+			port.write(snd)
+
 
 		print 'waiting...'
 		rcv=port.readline()
 		rcv=rcv.strip()
 		print "recieved data :",rcv
 		if len(rcv)>0:
-			temptr=int(rcv[1:2])
-			humidt=int(rcv[3:4])
-			moistr=int(rcv[5:6])
-			lite=int(rcv[7:8]) 
-			wc=int(rcv[9])
+			temptr=int(rcv[0:1])
+			humidt=int(rcv[2:3])
+			moistr=int(rcv[4:5])
+			lite=int(rcv[6:7]) 
+			srft=int(rcv[8:9]) #surface temp
+			lifm=int(rcv[10:11])   #leaf moisture
+
 			rtime=datetime.datetime.now()
 			con.execute('''' INSERT INTO Datas
-				(TIME,TEMP,HUMID,MOIST,LIGHT,WATER)
-				VALUES (?,?,?,?,?,?);''',(rtime,temptr,humidt,moistr,lite,wc))
+				(TIME,TEMP,HUMID,MOIST,LIGHT,SURFTEMP,LEAFMOIST)
+				VALUES (?,?,?,?,?,?,?);''',(rtime,temptr,humidt,moistr,lite,srft,lifm))
 			con.commit()
 con.close()
 port.close()			
